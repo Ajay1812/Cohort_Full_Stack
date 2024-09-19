2527b7b7ef148843faee964ea82f9080b0acf496
Ajay1812 <a.kumar01c@gmail.com>
2024-09-19 19:30:30 +0530

    react Dom and how it work

---
+++ JavaScript/week3/01-reconciler/test.js
@@ -0,0 +1,36 @@
+function findDiff(arr1, arr2){
+  let updated = 0, added = 0, deleted = 0
+  for (let i =0 ; i<arr2.length; i++){
+    let found = false
+    for(let j =0;j<arr1.length;j++){
+      if (arr1[j] === arr2[i]){
+        found = true
+      }
+    }
+    if (found){
+      updated ++
+    }
+    else{
+      added++
+    }
+  }
+  for(let i=0; i<arr1.length; i++){
+    let found = false
+    for(let j =0; j< arr2.length; j++){
+      if (arr1[i] === arr2[j]){
+        found = true
+      }
+    }
+    if (!found){
+      deleted ++
+    }
+  }
+  return {
+    updated: updated,
+    added : added,
+    deleted : deleted
+  }
+}
+
+
+console.log(findDiff([1,2,3,4], [1,2,3,4,5,6]))
---
+++ JavaScript/week4/map_filter.js
@@ -0,0 +1,22 @@
+// function multipleBy2(arr){
+//   let double = []
+//   for (let i =0 ; i< arr.length; i++){
+//     add = arr[i] * 2
+//     double.push(add)
+//   }
+//   return double
+// }
+
+let arr  = [1,2,3,4,5,6,2]
+
+// using forEach
+// arr.forEach((element, index, array)=> {
+//   array[index] = element * 2
+// })
+
+// using map
+// it will not return the array we have to store it in another variable
+doubleArr = arr.map((element) => element * 2) 
+console.log(doubleArr)
+
+// console.log(multipleBy2([1,2,3,4,5,6,2]))
